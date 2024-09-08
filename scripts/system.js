bcfee435b18a9cc099697d9df88f50c8afb6a922
Worldcompute <162045444+football24anketa@users.noreply.github.com>
2024-09-08 17:25:02 +0300

    1

--- scripts/system.js
+++ scripts/system.js
@@ -2,6 +2,7 @@ let scroll
 let body
 let rollercoaster
 let biglogo
+let lenta
 function handleResponse(data) {
     if (data.response && data.response[0].members_count) {
         console.log("Количество подписчиков: " + data.response[0].members_count);
@@ -19,9 +20,23 @@ window.onload = function(){
 	biglogo=document.getElementsByClassName('big-logo')[0]
 	rollercoaster = document.getElementsByClassName('rollercoaster');
 	rollercoaster[0].scrollTo({left: window.scrollY});
+	lenta=document.getElementsByClassName('stars_start')[0]
+	leaves=document.getElementsByClassName('leaves')[0]
+	if(lenta.getBoundingClientRect().y<=0){
+		leaves.style.opacity = '1';
+	}
+	else{
+		leaves.style.opacity = '0';
+	}
 };
 
 window.addEventListener('scroll', function(){
+	if(lenta.getBoundingClientRect().y<=0){
+		leaves.style.opacity = '1';
+	}
+	else{
+		leaves.style.opacity = '0';
+	}
 	scroll.style.marginTop = `-${window.scrollY/5}px`;
 	rollercoaster[0].scrollTo({left: window.scrollY});
 })
